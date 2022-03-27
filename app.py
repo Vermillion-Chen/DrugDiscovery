8bfc2e87f3b3c6fedee54c7e202f8a6dfadc6897
Victor <chenvictor247@gmail.com>
2022-03-27 04:15:59 -0400

    Basic landing page

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
+from unicodedata import name
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ ==  "__main__":
+    app.run()
